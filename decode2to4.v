df906e83d575b1c24cf6fbfe3db21b694ac5cc8c
MANOJ R <manojramesh2005@gmail.com>
2026-03-23 10:45:06 +0530

    Removed all files

--- decode2to4.v
+++
@@ -1,9 +0,0 @@
-module decode2to4(a,b,d1,d2,d3,d4);
-	input a,b;
-	output d1,d2,d3,d4;
-//hello
-	assign d1 = (~a & ~b);
-	assign d2 = (~a & b);
-	assign d3 = (a & ~b);
-	assign d4 = (a & b);
-endmodule
--- tri_buff.v
+++
@@ -1,8 +0,0 @@
-module tri_buff(
-	inout a,b,
-	input c);
-	
-		bufif1(b,a,c);
-		bufif0(a,b,c);
-	
-endmodule
