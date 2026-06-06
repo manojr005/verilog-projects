53cb83f03d388bf937d45cca5fce21e163744db3
MANOJ R <144418136+manojr005@users.noreply.github.com>
2026-06-06 09:22:21 +0530

    Fix non-blocking assignments in ud_load_counter

--- sequential_circuits/rtl/ud_load_counter.v
+++ sequential_circuits/rtl/ud_load_counter.v
@@ -5,9 +5,9 @@ module ud_load_counter(   //up down loadable 4 bit counter
 
 always@(posedge clk or negedge rst)
 begin
-	if(!rst) a=4'b0000;
-	else if(load) a = b;
-	else if(down) a = a-1; // if down is 1 it starts to count down
-	else a = a+1; // if down is not specified it count up 
+	if(!rst) a<=4'b0000;
+	else if(load) a <= b;
+	else if(down) a <= a-1; // if down is 1 it starts to count down
+	else a <= a+1; // if down is not specified it count up 
 end
 endmodule
